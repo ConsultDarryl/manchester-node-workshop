20e8cc7943c400ccc6507e736a4f91c889402422
Darryl Brown <darryl.brown@stagecoach-technology.com>
2018-01-19 14:37:40 +0000

    improve simple webserver example

--- 5-simple-webserver.js
+++ 5-simple-webserver.js
@@ -4,18 +4,19 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
 
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
+    var filename = "." + url.parse(req.url, true).pathname;
 
     fs.readFile(filename, function (err, data) {
         if (err) {
             res.writeHead(404, { 'Content-Type': 'text/html' });
-            return res.end("404 Not Found");
+            res.write("404 Not Found");
         }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
+        else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+        }
         return res.end();
     });
-    
+
 }).listen(8080);
 
