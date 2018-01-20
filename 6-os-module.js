759d28aa099e7e871831ca0348faa3810d30b0bd
Darryl <darryl.brown@stagecoach-technology.com>
2018-01-20 16:36:23 +0000

    final revisions

--- 6-os-module.js
+++ 6-os-module.js
@@ -1,3 +1,8 @@
 var os = require('os');
 console.log("Platform: " + os.platform());
+console.log("OS: " + os.type());
+console.log("OS Ver: " + os.release());
 console.log("Architecture: " + os.arch());
+console.log("CPU count: " + os.cpus().length);
+console.log("Free Memory: " + os.freemem());
+console.log("Total Memory: " + os.totalmem());
--- 7-custom-modules.js
+++ 7-custom-modules.js
@@ -1,8 +1,7 @@
 var http = require('http');
-var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write("The date and time are currently: " + dt.myDateTime());
+    res.write("The date and time are currently: ");
     res.end();
 }).listen(8080);
