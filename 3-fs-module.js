eed602c48fff57d2c0852e4a6fdc3eaf9cbd4207
Darryl Brown <darrylbrown@darrylbrown.co.uk>
2018-01-22 20:17:30 +0000

    rename demofile to index

--- 3-fs-module.js
+++ 3-fs-module.js
@@ -2,14 +2,14 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (request, response) {
-    fs.readFile('demofile1.html', function (err, data) {
+    fs.readFile('index.html', function (err, data) {
         response.writeHead(200, { 'Content-Type': 'text/html' });
         response.write(data);
         response.end();
     });
 }).listen(8080);
 
-// fs.readFile('demofile1.html', function (err, data) { });
+// fs.readFile('index.html', function (err, data) { });
 // fs.writeFile("myfile.txt", "Hey there!", function (err) { });
 // fs.appendFile('log.txt', 'abc', function (err) { });
 // fs.unlink("xyz"); // delete
--- 4-simple-webserver.js
+++ 4-simple-webserver.js
@@ -2,20 +2,20 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
-http.createServer(function (req, res) {
+http.createServer(function (request, response) {
 
-    var filename = "." + url.parse(req.url, true).pathname;
+    var filename = "." + url.parse(request.url, true).pathname;
 
     fs.readFile(filename, function (err, data) {
         if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.write("404 Not Found");
+            response.writeHead(404, { 'Content-Type': 'text/html' });
+            response.write("404 Not Found");
         }
         else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.write(data);
         }
-        return res.end();
+        return response.end();
     });
 
 }).listen(8080);
