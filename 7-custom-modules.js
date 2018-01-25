610a7765e9ecac53621b9962309cc2e6d2dd18db
Darryl <darryl.brown@stagecoach-technology.com>
2018-01-25 16:24:38 +0000

    post presentation updates

--- 7-custom-modules.js
+++ 7-custom-modules.js
@@ -1,7 +1,8 @@
 var http = require('http');
+var dt = require('./my-date-time');
 
 http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write("The date and time are currently: ");
+    response.write("The date and time are currently: " + dt.myDateTime());
     response.end();
 }).listen(8080);
