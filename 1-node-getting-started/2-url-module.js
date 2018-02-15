7c7a858c30aa2aef9935fc5a55b5b473377b7559
Darryl <darryl.brown@stagecoach-technology.com>
2018-02-15 11:27:13 +0000

    Updated URL module example

--- 1-node-getting-started/2-url-module.js
+++ 1-node-getting-started/2-url-module.js
@@ -1,12 +1,11 @@
-const url = require('url');
+const { URL } = require('url');
 
 var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
-var urlObject = url.parse(adr, true);
+var urlObject = new URL(adr);
 
 console.log(urlObject.host); //returns 'localhost:8080'
 console.log(urlObject.pathname); //returns '/default.htm'
 console.log(urlObject.search); //returns '?year=2017&month=february'
 
-var qdata = urlObject.query; //returns an object: { year: 2017, month: 'february' }
-console.log(qdata.month); //returns 'february'
+console.log(urlObject.searchParams.get('month')); //returns 'february'
 
